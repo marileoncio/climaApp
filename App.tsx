a3022c8516fa4f40bd7b6c60957fd6316cd29d09
marileoncio <mariana.timoteo19@yahoo.com>
2024-03-12 15:44:56 -0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,81 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
-function App(): React.JSX.Element{
-  return(
-    <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.city}>Presidente Epitácio</Text>
-        </View>
+function App(): React.JSX.Element {
+
+  const [city, setCity] = useState<string>("São Paulo");
+  const [humidity, setHumidity] = useState<string>("15");
+  const [condition, setCondition] = useState<string>("Ensolarado");
+  const [rainProbability, setRainProbability] = useState<string>("30");
+  const [feelsLike, setFeelsLike] = useState<string>("41");
+  const [temperature, setTemperature] = useState<string>("37");
+  const [night, setNight] = useState<boolean>(false);
+
+  function isNight() {
+    const hour = new Date().getHours();
+    console.log(hour);
+
+    if (hour >= 18 || hour < 6) {
+      setNight(true);
+    }else{
+      setNight(false);
+    }
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() =>{
+    isNight();
+  },6000);
+  })
+
+  return (
+    <View style={[styles.container, 
+    night == false ? styles.containerBgDay : styles.containerBgNight ]}>
+      <View style={styles.header}>
+        <Text style={styles.city}>{city}</Text>
+      </View>
 
       <View style={styles.detailsContainer}>
-        <Text style={styles.temperature}>55°C</Text>
-        <Image source={require('./src/assets/images/icon3.png')} style={styles.weatherIcon}/>
+        <Text style={styles.temperature}>{temperature} °C</Text>
+        <Image source={require('./src/assets/images/icon3.png')} style={styles.weatherIcon} />
       </View>
 
       <View>
-          <Text style={styles.weatherCondition}>Nublado</Text>
-          <Text style={styles.text}>Sensação Térmica: 179 °C</Text>
-          <Text style={styles.text}>Probabilidade de Chuva: 78°C</Text>
-          <Text style={styles.text}>Umidade: 99%</Text>
+        <Text style={styles.weatherCondition}>{condition}</Text>
+        <Text style={styles.text}>{feelsLike}°C</Text>
+        <Text style={styles.text}>{rainProbability}°C</Text>
+        <Text style={styles.text}>{humidity} %</Text>
       </View>
-          <Image source={require('./src/assets/images/cidade.png')} style={styles.bottomImage}/>
+      <Image source={require('./src/assets/images/cidade.png')} style={styles.bottomImage} />
     </View>
-      );
+  );
 }
 
 const styles = StyleSheet.create({
-    bottomImage: {
-      width: '115%',
-      position: 'absolute',
-      bottom: 0,
-      resizeMode: 'cover',
-      height: '57%'
-    },
-    header: {
-      flexDirection: 'row',
-      justifyContent:'space-between',
-      marginBottom: 20
-    },
-    city: {
-      fontWeight: 'bold',
-      color: '#19315F',
-      fontSize: 25
-    },
-    weatherIcon:{
-      width: 100,
-      height: 100,
-      alignSelf: 'flex-end',
-      marginBottom: 20,
-      flexDirection: 'row'
-    },
-    detailsContainer:{
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    weatherCondition:{
-      fontSize: 18,
-      fontWeight: 'bold',
-      alignSelf: 'flex-end',
-      marginBottom: 20,
-      color: '#19315F'
-     },
-    temperature:{
-      fontSize: 55,
-      marginBottom: 20,
-      color: '#99FEFF'
-    },
-    container:{
-      flex: 1,
-      padding: 20,
-      backgroundColor: '#00A8FF'
-    },
-    text:{
-      fontSize: 17,
-      color: '#19315F',
-      marginBottom: 10,
-      fontStyle: 'italic'
-    }
+  bottomImage: {
+    width: '115%',
+    position: 'absolute',
+    bottom: 0,
+    resizeMode: 'cover',
+    height: '57%'
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 20
+  },
+  city: {
+    fontWeight: 'bold',
+    color: '#99FEFF',
+    fontSize: 25
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
+    alignSelf: 'flex-end',
+    marginBottom: 20,
+    flexDirection: 'row'
+  },
+  detailsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  weatherCondition: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignSelf: 'flex-end',
+    marginBottom: 20,
+    color: '#99FEFF'
+  },
+  temperature: {
+    fontSize: 55,
+    marginBottom: 20,
+    color: '#99FEFF'
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#00A8FF'
+  },
+  text: {
+    fontSize: 17,
+    color: '#99FEFF',
+    marginBottom: 10,
+    fontStyle: 'italic'
+  },
+  containerBgDay: {
+    backgroundColor: '#09d3f3'
+  },
+  containerBgNight: {
+    backgroundColor: '#333'
+  }
 });
 
 export default App;
